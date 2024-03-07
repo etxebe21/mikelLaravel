17c6e003cfe1479960a1624692112e1e5ee13a55
MIKEL ETXEBE <mikelgartzia21@gmail.com>
2024-03-07 11:37:36 +0100

    [NEW] creado componente flash.blade

--- resources/views/examples.blade.php
+++ resources/views/examples.blade.php
@@ -5,5 +5,10 @@
     <h1 class="text-3xl">Listado de ejemplos</h1>
 
     <hr class="my-4">
+
+    <x-flash>Mensaje de OK</x-flash>
+    <x-flash type="error">Mensaje de ERROR</x-flash>
+    <x-flash type="info">Mensaje de INFO</x-flash>
+    <x-flash type="warning">Mensaje de ADVERTENCNIA</x-flash>
 </div>
 @endsection
