82c943ef9012e8309991fb0272c4ae0da749bc39
MIKEL ETXEBE <mikelgartzia21@gmail.com>
2024-03-08 13:24:00 +0100

    [NEW] creado componente card para mostrar tarjetas con imaganes y texto

--- resources/views/examples.blade.php
+++ resources/views/examples.blade.php
@@ -16,5 +16,28 @@
     <x-flash type="error"> Mensaje de ERROR</x-flash>
     <x-flash type="info"> Mensaje de INFO</x-flash>
     <x-flash type="warning"> Mensaje de ADVERTENCNIA</x-flash>
+
+    <hr class="my-4">
+    <div class="grid grid-cols-2 sm:grid-cols-3 gap-4">
+        <x-card
+            img="https://images.unsplash.com/photo-1623479322729-28b25c16b011"
+            date="Septiembre, 2015"
+            title="TITULO"
+            content="TITULO"
+        ></x-card>
+        <x-card
+            img="https://plus.unsplash.com/premium_photo-1661713438592-42a763d67cd6"            
+            date="Septiembre, 2015"
+            title="TITULO"
+            content="TITULO"
+        ></x-card>
+        <x-card
+            img="https://images.unsplash.com/photo-1629904853716-f0bc54eea481"
+            date="Septiembre, 2015"
+            title="TITULO"
+            content="TITULO que tiene esta tarjeta para comprobar si la card mantiene su tamaño"
+        ></x-card>
+    </div>
+
 </div>
 @endsection
